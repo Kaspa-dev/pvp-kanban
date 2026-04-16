6bb27fbc2d1ece1fa7e6d1df4b4d6e688e12690b
lukaskasparavic <lukaskasparavic@gmail.com>
2026-04-16 17:48:11 +0300

    feat: add planning poker persistence model

---
+++ BE/Models/PlanningPokerParticipant.cs
@@ -0,0 +1,16 @@
+namespace BE.Models;
+
+public class PlanningPokerParticipant
+{
+    public int Id { get; set; }
+    public int SessionId { get; set; }
+    public int? UserId { get; set; }
+    public string GuestToken { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public bool IsHost { get; set; }
+    public bool IsGuest { get; set; }
+    public DateTime LastSeenAtUtc { get; set; }
+
+    public PlanningPokerSession Session { get; set; } = null!;
+    public User? User { get; set; }
+}
